16877f622b11e241bb340bb8be30d7a8572ca392
azamatzhumakeev <azamatzhumakeev@10.1.10.174>
2018-03-30 23:01:40 -0500

    Azamat

---
+++ src/test/java/com/app/runners/CukesRunner.java
@@ -0,0 +1,17 @@
+package com.app.runners;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+
+@CucumberOptions(
+		plugin = {"pretty", 
+				"html:target/cucumber-report"
+		},
+		tags = "@dev",
+		features="src/test/resources/com/app/features/",
+		glue="com/app/step_definitions/",
+		dryRun=false 
+)
+public class CukesRunner extends AbstractTestNGCucumberTests {
+
+}
